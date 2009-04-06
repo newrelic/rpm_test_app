124a2ad0459e6c84816ac859511346c94b450b2d
Bill Kayser <bkayser@newrelic.com>
2009-04-06 08:55:01 -0700

    Removing extra stuff

--- vendor/gems/newrelic_rpm-2.9.0/lib/new_relic/agent/instrumentation/rails/rails.rb
+++
@@ -1,8 +0,0 @@
-# Instrument the compilation of ERB files.
-# For developer mode, we subtract compile from total TT time
-=begin
-ERB::Compiler.class_eval do
-  add_method_tracer :compile, 'View/.rhtml Processing'
-end
-=end
-
