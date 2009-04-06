66ea1ff09d806ee13b2964d828e24ddf365d1875
Bill Kayser <bkayser@newrelic.com>
2009-04-06 08:51:33 -0700

    Removing hardwired gem

--- vendor/gems/newrelic_rpm-2.9.0/lib/new_relic/agent/instrumentation/rails/rails.rb
+++ vendor/gems/newrelic_rpm-2.9.0/lib/new_relic/agent/instrumentation/rails/rails.rb
@@ -1,7 +1,8 @@
 # Instrument the compilation of ERB files.
 # For developer mode, we subtract compile from total TT time
+=begin
 ERB::Compiler.class_eval do
   add_method_tracer :compile, 'View/.rhtml Processing'
 end
-
+=end
 
