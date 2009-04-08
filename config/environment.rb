3cbc912645dedefbd03776f13d09c7af41e6710b
Bill Kayser <bkayser@newrelic.com>
2009-04-08 11:10:06 -0700

    fix error

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,11 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.1.2' #unless defined? RAILS_GEM_VERSION
+<<<<<<< HEAD
 #RAILS_GEM_VERSION = '2.3.0' unless defined? RAILS_GEM_VERSION
 
+=======
+>>>>>>> fix error
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
