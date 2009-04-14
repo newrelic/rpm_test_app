923b2198194f1c49ebbd340361f1f8da0727babe
Bill Kayser <bkayser@newrelic.com>
2009-04-14 15:15:23 -0700

    use plugin

--- config/environment.rb
+++ config/environment.rb
@@ -12,7 +12,7 @@ RAILS_GEM_VERSION = '2.3.2' #unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-config.gem "newrelic_rpm"
+#config.gem "newrelic_rpm"
 #config.gem "newrelic_rpm", :version => '2.8.1'
 #config.gem "newrelic_rpm", :version => '2.9.0'
   # Settings in config/environments/* take precedence over those specified here.
