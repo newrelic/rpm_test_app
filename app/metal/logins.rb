d847bca7e22f13dc9b1141edb4209eec66c1ffc8
Bill Kayser <bkayser@newrelic.com>
2009-10-25 12:44:51 -0700

    tune the middleware

--- app/metal/logins.rb
+++ app/metal/logins.rb
@@ -1,6 +1,6 @@
 # Allow the metal piece to run in isolation
 require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
-
+require 'new_relic/agent/instrumentation/rack'
 class Logins
   def self.call(env)
     if env["PATH_INFO"] =~ /^\/logins/
@@ -9,4 +9,5 @@ class Logins
       [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
   end
+  extend NewRelic::Agent::Instrumentation::Rack
 end
