1ad2beb50ae0502c64e420405e4c97ece68fe19b
Bill Kayser <bkayser@newrelic.com>
2010-02-09 15:33:25 -0800

    fix script

--- script/hudson.sh
+++ script/hudson.sh
@@ -10,7 +10,7 @@ mkdir -p vendor/plugins
 svn export http://repo.newrelic.com/rpm/projects/Agent/trunk tmp/newrelic_rpm
 
 (cd tmp/newrelic_rpm; $RUBY -S rake build )
-$RUBY -S gem install vendor/newrelic_rpm/pkg/*.gem -i vendor --no-rdoc --no-ri
+$RUBY -S gem install tmp/newrelic_rpm/pkg/*.gem -i vendor --no-rdoc --no-ri
 export RAILS_ENV=test
 
 $RUBY -S rake ci:setup:testunit test:newrelic || echo "Unit test failures"
