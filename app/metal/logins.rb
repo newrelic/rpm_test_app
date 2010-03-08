bc00c6e85fae04b1f050dd7f4ba0d3ffe92beac5
Bill Kayser <bkayser@newrelic.com>
2010-03-08 10:28:08 -0800

    Throw an error with /logins/ANYTHING

--- app/metal/logins.rb
+++ app/metal/logins.rb
@@ -1,8 +1,11 @@
 require 'new_relic/agent/instrumentation/rack'
 class Logins
   def self.call(env)
-    if env["PATH_INFO"] =~ /^\/logins/
+    if env["PATH_INFO"] =~ /^\/logins$/
       [200, {"Content-Type" => "text/html"}, ["<html><pre>#{%x[who]}</pre></html>"]]
+    elsif env["PATH_INFO"] =~ /^\/logins.+$/
+      puts "uri: #{NewRelic::Agent::Instrumentation::MetricFrame.current.uri}"
+      raise "This is an error with the path!"
     else
       [404, {"Content-Type" => "text/html"}, ["Not Found"]]
     end
