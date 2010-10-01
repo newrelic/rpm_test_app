48cbebcb50eefdee6192ddfdc58b73be2af22e9d
Justin George <justin.george@gmail.com>
2010-09-30 17:12:01 -0700

    update to use integration branch

--- script/hudson.sh
+++ script/hudson.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Script executed by hudson
-echo "Run in $(pwd)"
-echo "Script is $0"
-
-git checkout origin/$BRANCH
+echo "Executing $0"
+echo "Running in $(pwd)"
 rm -rf tmp/newrelic_rpm vendor/plugins/newrelic_rpm vendor/gems vendor/newrelic_rpm
 mkdir -p tmp
 mkdir -p log
@@ -14,11 +12,12 @@ source ~/.rvm/scripts/rvm
 
 rvm $RUBY
 
-git clone hudson@repo.newrelic.com:/git/ruby_agent.git vendor/plugins/newrelic_rpm
+git clone hudson@repo.newrelic.com:/git/ruby_agent.git tmp/newrelic_rpm
 
-(cd vendor/plugins/newrelic_rpm; git checkout integration; cd ../../..)
+(cd tmp/newrelic_rpm; git checkout integration; rake build )
+gem install tmp/newrelic_rpm/pkg/*.gem -i vendor --no-rdoc --no-ri
 export RAILS_ENV=test
 
-#rake gems:install
+rake gems:install
 rake --trace ci:setup:testunit test:newrelic || echo "Unit test failures"
 
