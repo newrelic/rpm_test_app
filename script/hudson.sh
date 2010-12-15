debcd53ed029082d7b9409fc243374909ad37ad7
Justin George <justin.george@gmail.com>
2010-12-14 17:17:28 -0800

    update hudson script and lockfile

--- script/hudson.sh
+++ script/hudson.sh
@@ -12,12 +12,7 @@ source ~/.rvm/scripts/rvm
 
 rvm $RUBY
 
-git clone hudson@repo.newrelic.com:/git/ruby_agent.git tmp/newrelic_rpm
-
-(cd tmp/newrelic_rpm; git checkout -b integration origin/integration; rake build )
-gem install tmp/newrelic_rpm/pkg/*.gem -i vendor --no-rdoc --no-ri
 export RAILS_ENV=test
 
-rake gems:install
 rake --trace ci:setup:testunit test:newrelic || echo "Unit test failures"
 
