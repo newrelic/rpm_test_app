6170f819ab2221cf8d3c50964474ee51f1b3097e
Justin George <justin.george@gmail.com>
2011-02-25 13:37:49 -0800

    rack? really? that isnt installed by default

--- script/hudson.sh
+++ script/hudson.sh
@@ -21,7 +21,7 @@ rvm gemset use rails22_agent_tests
 
 gem install rails --version=2.2.2
 
-gem install jeweler bundler shoulda mocha
+gem install jeweler bundler shoulda mocha rack
 
 git clone hudson@repo.newrelic.com:/git/ruby_agent.git tmp/newrelic_rpm
 
