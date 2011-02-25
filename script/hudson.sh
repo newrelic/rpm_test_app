9d472c85eaa2cb22101126492c3d423fc81b0ca3
Justin George <justin.george@gmail.com>
2011-02-25 13:08:02 -0800

    force gemset empty before we start

--- script/hudson.sh
+++ script/hudson.sh
@@ -12,6 +12,8 @@ source ~/.rvm/scripts/rvm
 
 rvm $RUBY@rails20_agent_tests --create
 
+rvm --force gemset empty rails20_agent_tests
+
 gem install rails --version=2.0.2
 
 git clone hudson@repo.newrelic.com:/git/ruby_agent.git tmp/newrelic_rpm
