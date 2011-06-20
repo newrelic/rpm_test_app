1ec904cdda004a97073dd1a2055042b97c86eb44
Jon Guymon <jguymon@igicom.com>
2011-06-20 16:04:18 -0700

    no more named_scope, now just scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :blog
-  named_scope :published, :conditions => {:published => true}
+  scope :published, :conditions => {:published => true}
 
   after_save do
     sleep 5
