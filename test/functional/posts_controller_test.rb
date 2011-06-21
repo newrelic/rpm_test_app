a12c8c0f490474f0e063568efb60636366fb26f9
Jon Guymon <jguymon@igicom.com>
2011-06-21 15:11:47 -0700

    tests pass

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -2,44 +2,44 @@ require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
   test "should get index" do
-    get :index
+    get :index, :blog_id => blogs(:one)
     assert_response :success
     assert_not_nil assigns(:posts)
   end
 
   test "should get new" do
-    get :new
+    get :new, :blog_id => blogs(:one)
     assert_response :success
   end
 
   test "should create post" do
     assert_difference('Post.count') do
-      post :create, :post => { }
+      post :create, :post => { }, :blog_id => blogs(:one)
     end
 
-    assert_redirected_to post_path(assigns(:post))
+    assert_redirected_to blog_path(assigns(:blog))
   end
 
   test "should show post" do
-    get :show, :id => posts(:one).id
+    get :show, :id => posts(:one).id, :blog_id => blogs(:one)
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, :id => posts(:one).id
+    get :edit, :id => posts(:one).id, :blog_id => blogs(:one)
     assert_response :success
   end
 
   test "should update post" do
-    put :update, :id => posts(:one).id, :post => { }
-    assert_redirected_to post_path(assigns(:post))
+    put :update, :id => posts(:one).id, :post => { }, :blog_id => blogs(:one)
+    assert_redirected_to blog_post_path(assigns(:post))
   end
 
   test "should destroy post" do
     assert_difference('Post.count', -1) do
-      delete :destroy, :id => posts(:one).id
+      delete :destroy, :id => posts(:one).id, :blog_id => blogs(:one)
     end
 
-    assert_redirected_to posts_path
+    assert_redirected_to blog_posts_path
   end
 end
